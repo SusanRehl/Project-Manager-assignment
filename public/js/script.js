f168b2e7cf6b7fb4850fd644e32d1b0c4bed9821
anna baker <annakatherinebaker@gmail.com>
2016-06-09 14:00:55 -0500

    company button completed

--- public/js/script.js
+++ public/js/script.js
@@ -11,8 +11,29 @@ $(document).ready(function(){
     }
   });
 
+var companyName = [ 'Target', 'Best Buy', 'General Mills', 'Medtronic', 'Dairy Queen',
+  '3M', 'EcoLab', 'Uponor', 'Lynx', 'The Wild', 'University of Minnesota' ];
+
+function getRandomInt( min, max ){
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+var projectNeeds = {
+  frontEnd: '',
+  clientSide: '',
+  serverLogic: ''
+};
+
+console.log(projectNeeds);
+
 $('#generateCompany').on('click', function(){
 //append company name to the dom
+var newCompanyName = companyName[getRandomInt(0, companyName.length - 1)];
+projectNeeds = {
+  frontEnd: getRandomInt(10, 60),
+  clientSide: getRandomInt(10, 60),
+  serverLogic: getRandomInt(10, 60)
+};
+
 var assignStaff;
 if ($('.assignStaff').length){
 
@@ -22,8 +43,14 @@ if ($('.assignStaff').length){
   assignStaff.className = 'assignStaff';
   console.log("In IF");
   assignStaff.innerHTML = 'Assign Staff';
-  document.body.appendChild( assignStaff );
-  alert( companyName[getRandomInt(0, companyName.length - 1)] );
+  document.getElementById('nav').appendChild( assignStaff );
+  // alert( companyName[getRandomInt(0, companyName.length - 1)] );
+
+  $('#companyName').text(newCompanyName);
+  $('#frontEnd').text("Front End: " + projectNeeds.frontEnd);
+  $('#clientSide').text("Client Side Logic: " + projectNeeds.clientSide);
+  $('#serverLogic').text("Server Side Logic: " + projectNeeds.serverLogic);
+
 
 } else{
   console.log("in else");
@@ -31,9 +58,13 @@ if ($('.assignStaff').length){
   assignStaff.className = 'assignStaff';
 
   assignStaff.innerHTML = 'Assign Staff';
-  document.body.appendChild( assignStaff );
-  alert( companyName[getRandomInt(0, companyName.length - 1)] );
+  document.getElementById('nav').appendChild( assignStaff );
+  // alert( companyName[getRandomInt(0, companyName.length - 1)] );
 
+  $('#companyName').text(newCompanyName);
+  $('#frontEnd').text("Front End: " + projectNeeds.frontEnd);
+  $('#clientSide').text("Client Side Logic: " + projectNeeds.clientSide);
+  $('#serverLogic').text("Server Side Logic: " + projectNeeds.serverLogic);
 }
 
 });
@@ -42,9 +73,7 @@ if ($('.assignStaff').length){
 
 });
 
-var companyName = [ 'Target', 'Best Buy', 'General Mills', 'Medtronic', 'Dairy Queen',
-'3M', 'EcoLab', 'Uponor', 'Lynx', 'The Wild', 'University of Minnesota' ];
 
-function getRandomInt( min, max ){
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+
+console.log(projectNeeds);
+console.log(projectNeeds.frontEnd);
